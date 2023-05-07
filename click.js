346a4730d0af484885bdda4af1b5643c079e4fa1
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-05-07 15:25:19 +0530

    sounds played when click on div

---
+++ click.js
@@ -0,0 +1,23 @@
+const clap=new Audio();
+  clap.src="sounds/clap.wav";
+
+const boom=new Audio();
+  boom.src="sounds/boom.wav";
+
+const hihat=new Audio();
+  hihat.src="sounds/hihat.wav";
+
+const kick=new Audio();
+  kick.src="sounds/kick.wav";
+
+const openhat=new Audio();
+  openhat.src="sounds/openhat.wav";
+
+const ride=new Audio();
+  ride.src="sounds/ride.wav";
+
+const tank=new Audio();
+  tank.src="sounds/tank.wav";
+
+const tom=new Audio();
+  tom.src="sounds/tom.wav";
