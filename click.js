52fd715432d8f199746e92657d273d5b50146d1b
Yash-Beldar <yashodip2003beldar@gmail.com>
2023-09-12 11:42:31 +0530

    modified clicking process

--- click.js
+++ click.js
@@ -21,3 +21,19 @@ const tank=new Audio();
 
 const tom=new Audio();
   tom.src="sounds/tom.wav";
+
+  // <!-- <div class="container-fluid">
+  // <nav class="navbar navbar-inverse">
+  //   <button class="navbar-toggler btn"yy type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  //     <i class="fas fa-bars"></i>
+  //   </button>
+  //   <div class="container-fluid collapse navbar-collapse" id="navbarSupportedContent">
+  //     <ul class="nav navbar-nav">
+  //       <li><a id="len1" class="hoverable" href="#">Home</a></li>
+  //       <li><a id="len2" class="hoverable" href="#">About</a></li>
+  //       <li><a id="len3" class="hoverable" href="#">Portfolio</a></li>
+  //       <li><a id="len4" class="hoverable" href="#">Contact</a></li>
+  //     </ul>
+  //   </div>
+  // </nav>
+  // </div> -->
